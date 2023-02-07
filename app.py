05d03e7f40ba45d27168a73f7d25e2c3c537777c
Feodor <feodor1999@mail.ru>
2023-02-07 15:33:17 +0300

    добавил стили

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ config.read('./config.ini', encoding='utf-8-sig')
 app = Flask(__name__, instance_relative_config=True)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+table = {
+    'НОМЕР ВАГОНА', 'КОД', 'НОМЕР СОСТАВА','СУБГР'
+}
+
 class Database:
     def __init__(self):
         self.con = cx_Oracle.connect(f"{config.get('db', 'dbu')}/{config.get('db', 'dbp')}@//{config.get('db', 'host')}/{config.get('db', 'name')}")
@@ -22,7 +26,7 @@ def main():
     def db_query():
         db = Database()
         return db.list_of_vagon()
-    return render_template('index.html', data=db_query())
+    return render_template('index.html', data=db_query(), table=table)
 
 if __name__ == "__main__":
     app.run(debug=True)

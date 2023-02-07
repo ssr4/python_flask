38931cb5900dc9844421be17d5dcd72bd34fd010
Feodor <feodor1999@mail.ru>
2023-02-07 13:45:35 +0300

    вывод строк из бд

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template
+import cx_Oracle, configparser
 # from livereload import Server
 
-app = Flask(__name__, instance_relative_config=True,)
+config = configparser.ConfigParser()
+config.read('./config.ini', encoding='utf-8-sig')
+
+app = Flask(__name__, instance_relative_config=True)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-data=[
-    {
-        'name':'Audrin',
-    },
-    {
-        'name':'Fedya12',
-    }
-]
-
+class Database:
+    def __init__(self):
+        self.con = cx_Oracle.connect(f"{config.get('db', 'dbu')}/{config.get('db', 'dbp')}@//{config.get('db', 'host')}/{config.get('db', 'name')}")
+        self.cur = self.con.cursor()
+    def list_of_vagon(self):
+        self.cur.execute('select * from own_vag')
+        return self.cur.fetchmany(10)
 
 @app.route("/")
 
 def main():
-    return render_template('index.html', data=data)
+    def db_query():
+        db = Database()
+        return db.list_of_vagon()
+    return render_template('index.html', data=db_query())
 
 if __name__ == "__main__":
     app.run(debug=True)

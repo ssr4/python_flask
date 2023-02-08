19b6c31d54f285839dfb55d34513d031434c8f40
Feodor <feodor1999@mail.ru>
2023-02-08 13:45:58 +0300

    переход на другие страницы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,flash, redirect
 import cx_Oracle, configparser
 # from livereload import Server
 
@@ -7,6 +7,7 @@ config.read('./config.ini', encoding='utf-8-sig')
 
 app = Flask(__name__, instance_relative_config=True)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SECRET_KEY'] = 'yo0ecrasdasdasdeey'
 
 table = {
     'НОМЕР ВАГОНА', 'КОД', 'НОМЕР СОСТАВА','СУБГР'
@@ -18,16 +19,34 @@ class Database:
         self.cur = self.con.cursor()
     def list_of_vagon(self):
         self.cur.execute('select * from own_vag')
-        return self.cur.fetchmany(10)
+        return self.cur.fetchmany(15)
+    def count_of_vagons(self):
+        self.cur.execute('select COUNT(*) from own_vag')
+        return self.cur.fetchone()
 
 @app.route("/")
-
 def main():
+    return render_template('index.html', table=table)
+
+
+@app.route("/vagons")
+def vagons():
     def db_query():
         db = Database()
         return db.list_of_vagon()
-    return render_template('index.html', data=db_query(), table=table)
+    def db_count():
+        db = Database()
+        return db.count_of_vagons()[0]
+    return render_template('vagons.html', data=db_query(),count = db_count(), table=table)
+
+@app.route("/from_file")
+def from_file():
+    def db_query():
+        db = Database()
+        return db.count_of_vagons()
+    return render_template('excel.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    

ef590cb33e7833858bf222194bbf93da574c84bb
Feodor <feodor1999@mail.ru>
2023-02-10 13:01:38 +0300

    проверка на вагоны с прерырванием

--- static/js/script.js
+++ static/js/script.js
@@ -18,14 +18,27 @@ function myFunction() {
     .then((response) => {
       return response.json()
     })
-    .then((content) => {
+    .then((vagons) => {
       let count = 0
-      content.forEach((vagon) => {
-        // проверка что в строке исключительно числа
-        if (!/\D/.test(vagon)) {
-          if (vagon.length === 8) count++
+      // content.forEach((vagon) => {
+      //   // проверка что в строке исключительно числа
+      //   if (!/\D/.test(vagon)) {
+      //     if (vagon.length === 8) count++
+      //   } else {
+      //     if (confirm(`Это не номер вагона ${vagon}. Продолжить?`)) return
+      //   }
+      // })
+      let i = 0,
+        answer = true
+      do {
+        i++
+        if (!/\D/.test(vagons[i])) {
+          if (vagons[i].length === 8) count++
+        } else {
+          if (!confirm(`Это не номер вагона ${vagons[i]}. Продолжить?`))
+            answer = false
         }
-      })
+      } while (answer && i < vagons.length)
       console.log('количество вагонов ', count)
     })
 }
